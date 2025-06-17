669a0418ad9beb2da14aee166360429c08b734b8
MigoCode66 <migocode66@gmail.com>
2025-06-17 19:13:39 +0200

    note feching form db added

--- web/app/firebase/config.ts
+++ web/app/firebase/config.ts
@@ -1,23 +1,29 @@
-'server only';
-
+'use client';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  setPersistence,
-} from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// firebase config
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFyR-EYfsA46R12klpt0HaNhXQgYsP0A0',
-  authDomain: 'rewinote-b4d24.firebaseapp.com',
-  projectId: 'rewinote-b4d24',
-  storageBucket: 'rewinote-b4d24.firebasestorage.app',
-  messagingSenderId: '392299206633',
-  appId: '1:392299206633:web:23feced34e084ee2cd1d78',
-};
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase configuration
+const firebaseConfig = process.env.FIREBASE_CONFIG
+  ? JSON.parse(process.env.FIREBASE_CONFIG)
+  : {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
